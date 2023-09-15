87b647a537a5b6e6e4d8cc0d1b3e7afc06985b77
Maximiliano Greco <mmngreco@gmail.com>
2023-09-15 08:59:46 +0200

    apply format

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,69 +7,69 @@ local executable = function(x)
 end
 
 return {
-    'sbulav/nredir.nvim',
-    'tpope/vim-abolish',
-    'tpope/vim-dadbod',
-    'kristijanhusak/vim-dadbod-ui',
-    'tpope/vim-dispatch',
-    'tpope/vim-fugitive',
-    'tpope/vim-markdown',
-    'tpope/vim-obsession',
-    'tpope/vim-repeat',
-    'tpope/vim-rhubarb',
-    'tpope/vim-speeddating',
-    'tpope/vim-surround',
-    'tpope/vim-unimpaired',
-    'tpope/vim-vinegar',
-    'goerz/jupytext.vim',
-    'ThePrimeagen/harpoon',
-    'szw/vim-maximizer',
-    'RRethy/vim-illuminate',
-    'jpalardy/vim-slime',
-    'mhartington/formatter.nvim',
-    'tyru/open-browser.vim',
-    {
-        "zbirenbaum/copilot.lua",
-        config = function()
-            require('copilot').setup()
-        end,
-    },
+  'sbulav/nredir.nvim',
+  'tpope/vim-abolish',
+  'tpope/vim-dadbod',
+  'kristijanhusak/vim-dadbod-ui',
+  'tpope/vim-dispatch',
+  'tpope/vim-fugitive',
+  'tpope/vim-markdown',
+  'tpope/vim-obsession',
+  'tpope/vim-repeat',
+  'tpope/vim-rhubarb',
+  'tpope/vim-speeddating',
+  'tpope/vim-surround',
+  'tpope/vim-unimpaired',
+  'tpope/vim-vinegar',
+  'goerz/jupytext.vim',
+  'ThePrimeagen/harpoon',
+  'szw/vim-maximizer',
+  'RRethy/vim-illuminate',
+  'jpalardy/vim-slime',
+  'mhartington/formatter.nvim',
+  'tyru/open-browser.vim',
+  {
+    "zbirenbaum/copilot.lua",
+    config = function()
+      require('copilot').setup()
+    end,
+  },
 
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        dependencies = { "copilot.lua" },
-        config = function()
-            require("copilot_cmp").setup()
-        end,
-    },
-    {
-        "NTBBloodbath/rest.nvim",
-        enable = executable "jq",
-        config = function()
-            require("rest-nvim").setup()
-        end,
-    },
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "nvim-neotest/neotest-vim-test",
-            "nvim-neotest/neotest-python",
-            "nvim-neotest/neotest-plenary",
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim"
-        }
-    },
-    {
-        "klen/nvim-test",
-        config = function()
-            require('nvim-test').setup({})
-        end
-    },
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    dependencies = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+  {
+    "NTBBloodbath/rest.nvim",
+    enable = executable "jq",
+    config = function()
+      require("rest-nvim").setup()
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/neotest-vim-test",
+      "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-plenary",
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim"
+    }
+  },
+  {
+    "klen/nvim-test",
+    config = function()
+      require('nvim-test').setup({})
+    end
+  },
   {
     'jakewvincent/mkdnflow.nvim',
     rocks = 'luautf8', -- Ensures optional luautf8 dependency is installed
@@ -92,6 +92,11 @@ return {
   },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = { "mfussenegger/nvim-dap",'theHamsta/nvim-dap-virtual-text', 'mfussenegger/nvim-dap-python' }
+    dependencies = {
+      "mfussenegger/nvim-dap",
+			'theHamsta/nvim-dap-virtual-text',
+			'mfussenegger/nvim-dap-python',
+    }
   },
 }
+-- vim: ts=2 sts=2 sw=2 et tw=0
