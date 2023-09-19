5d54da39ff2bcffcb0c53fb59666dcde16153439
Maximiliano Greco <mmngreco@gmail.com>
2023-09-19 18:35:17 +0200

    update

--- init.lua
+++ init.lua
@@ -644,11 +644,10 @@ vim.o.completeopt = 'menuone,noselect'
 vim.g.netrw_hide = 0
 vim.o.laststatus = 3
 
--- TODO: fix this
 function Gpt_command(args)
+  -- vim.print(args)
   local mod = args.fargs[1]
   local prompt = ""
-
   for key, value in ipairs(args.fargs) do
     if key > 1 then
       prompt = prompt .. value .. " "
@@ -657,13 +656,13 @@ function Gpt_command(args)
   local filetype = vim.bo.filetype
   local prefix = 'You are a ' .. filetype .. ' developer/writer expert. '
   prefix = prefix .. 'Avoid comments and explanations unless I ask for it. '
-  prefix = prefix .. 'Keep the original language. '
+  prefix = prefix .. 'You should make changes using the same language as the input.'
   local cmd = ''
   if prompt == nil then
     prompt = ''
   end
-
   local _cmd = ''
+  -- detect visual mode
   if mod == '%' then
     -- file
     _cmd = '%!chatgpt -p "'
@@ -671,18 +670,26 @@ function Gpt_command(args)
     -- line
     _cmd = '.!chatgpt -p "'
   else
-    _cmd = '!chatgpt -p "'
+    prompt = mod .. " " .. prompt
+    if args.line1 ~= nil then
+        -- visual
+        -- vim.print("visual mode")
+        _cmd = '\'<,\'>!chatgpt -p "'
+    else
+        -- vim.print("normal mode")
+        _cmd = '!chatgpt -p "'
+    end
   end
-  cmd = _cmd .. prefix .. prompt .. '"<cr>'
-  vim.print(cmd)
-  vim.api.nvim_cmd({ cmd }, { output = true })
+  cmd = _cmd .. prefix .. prompt .. '"'
+  -- debug
+  -- vim.print(cmd)
+  vim.cmd(cmd)
 end
+vim.api.nvim_create_user_command('Gpt', Gpt_command, { nargs = "*", range = true })
 
--- hla mundo
-
-vim.api.nvim_set_keymap('n', '<C-c>f', ':%!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '%!chatgpt' })
-vim.api.nvim_set_keymap('n', '<C-c>.', ':.!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '.!chatgpt' })
-vim.api.nvim_set_keymap('v', '<C-c>.', ':!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '!chatgpt' })
+-- vim.api.nvim_set_keymap('n', '<C-c>f', ':%!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '%!chatgpt' })
+-- vim.api.nvim_set_keymap('n', '<C-c>.', ':.!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '.!chatgpt' })
+-- vim.api.nvim_set_keymap('v', '<C-c>.', ':!chatgpt -p "Avoid comments and explanaitions unless I ask for it. "<left>', { noremap = true, desc = '!chatgpt' })
 
 -- Explore
 vim.keymap.set('n', '<leader>-', ':Ex %:h<cr>', { desc = "Open the current file's directory in the file explorer", silent = false })
@@ -1261,6 +1268,20 @@ require("formatter").setup {
   }
 }
 -- }}
+
+
+
+
+
+function Bang_command()
+  local cmd = ":.!sh"
+  local out = vim.cmd(cmd)
+  vim.print(out)
+end
+vim.api.nvim_create_user_command('Bang', Bang_command, { nargs = "*" })
+
+-- hola
+
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et tw=0
