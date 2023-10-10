f351bc1afb25f1694476d158fa19448543fe4d1d
Maximiliano Greco <mmngreco@gmail.com>
2023-10-10 17:49:03 +0200

    Auto commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -88,7 +88,7 @@ return {
       require('copilot').setup()
     end,
   },
-
+  -- { 'github/copilot.vim' },
   {
     'hrsh7th/nvim-cmp',
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
