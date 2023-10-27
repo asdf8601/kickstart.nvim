9c0b4d69214011c4889e0b0a4a0722183a809b23
Maximiliano Greco <mmngreco@gmail.com>
2023-10-27 18:55:08 +0200

    Auto commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,6 +9,34 @@ local executable = function(x)
 end
 
 return {
+  {
+    'Pocco81/HighStr.nvim',
+    init = function ()
+      local high_str = require("high-str")
+      high_str.setup({
+        verbosity = 0,
+        saving_path = "/tmp/highstr/",
+        highlight_colors = {
+          -- color_id = {"bg_hex_code",<"fg_hex_code"/"smart">}
+          color_0 = {"#0c0d0e", "smart"},	-- Cosmic charcoal
+          color_1 = {"#e5c07b", "smart"},	-- Pastel yellow
+          color_2 = {"#7FFFD4", "smart"},	-- Aqua menthe
+          color_3 = {"#8A2BE2", "smart"},	-- Proton purple
+          color_4 = {"#FF4500", "smart"},	-- Orange red
+          color_5 = {"#008000", "smart"},	-- Office green
+          color_6 = {"#0000FF", "smart"},	-- Just blue
+          color_7 = {"#FFC0CB", "smart"},	-- Blush pink
+          color_8 = {"#FFF9E3", "smart"},	-- Cosmic latte
+          color_9 = {"#7d5c34", "smart"},	-- Fallow brown
+        }
+      })
+      vim.api.nvim_set_keymap( "v", "<leader>h1", ":<c-u>HSHighlight 1<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap( "v", "<leader>h2", ":<c-u>HSHighlight 2<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap( "v", "<leader>h3", ":<c-u>HSHighlight 3<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap( "v", "<leader>h4", ":<c-u>HSHighlight 4<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap( "v", "<leader>h0", ":<c-u>HSRmHighlight<CR>", { noremap = true, silent = true })
+    end
+  },
     {
       "ray-x/go.nvim",
       dependencies = {  -- optional packages
