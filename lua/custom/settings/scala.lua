5adbcde7ed83b82ae1d53267e9c55712c33acaec
Maximiliano Greco <mmngreco@gmail.com>
2023-10-27 14:57:48 +0200

    Auto commit

--- lua/custom/settings/scala.lua
+++ lua/custom/settings/scala.lua
@@ -7,45 +7,45 @@ metals_config.settings = {
   excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
--- *READ THIS*
--- I *highly* recommend setting statusBarProvider to true, however if you do,
--- you *have* to have a setting to display this in your statusline or else
--- you'll not see any messages from metals. There is more info in the help
--- docs about this
--- metals_config.init_options.statusBarProvider = "on"
-
--- Example if you are using cmp how to make sure the correct capabilities for snippets are set
-metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
-
--- Debug settings if you're using nvim-dap
-local dap = require("dap")
-
-dap.configurations.scala = {
-  {
-    type = "scala",
-    request = "launch",
-    name = "RunOrTest",
-    metals = {
-      runType = "runOrTestFile",
-      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
-    },
-  },
-  {
-    type = "scala",
-    request = "launch",
-    name = "Test Target",
-    metals = {
-      runType = "testTarget",
-    },
-  },
-}
-
-metals_config.on_attach = function(client, bufnr)
-  require("metals").setup_dap()
-end
+-- -- *READ THIS*
+-- -- I *highly* recommend setting statusBarProvider to true, however if you do,
+-- -- you *have* to have a setting to display this in your statusline or else
+-- -- you'll not see any messages from metals. There is more info in the help
+-- -- docs about this
+-- -- metals_config.init_options.statusBarProvider = "on"
+--
+-- -- Example if you are using cmp how to make sure the correct capabilities for snippets are set
+-- metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+--
+-- -- Debug settings if you're using nvim-dap
+-- local dap = require("dap")
+--
+-- dap.configurations.scala = {
+--   {
+--     type = "scala",
+--     request = "launch",
+--     name = "RunOrTest",
+--     metals = {
+--       runType = "runOrTestFile",
+--       --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+--     },
+--   },
+--   {
+--     type = "scala",
+--     request = "launch",
+--     name = "Test Target",
+--     metals = {
+--       runType = "testTarget",
+--     },
+--   },
+-- }
+--
+-- metals_config.on_attach = function(client, bufnr)
+--   require("metals").setup_dap()
+-- end
 
 -- Autocmd that will actually be in charging of starting the whole thing
-local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
+local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
 vim.api.nvim_create_autocmd("FileType", {
   -- NOTE: You may or may not want java included here. You will need it if you
   -- want basic Java support but it may also conflict if you are using
