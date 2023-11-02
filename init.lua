72a93be49080b317bbeffc54402800ada47ec026
Maximiliano Greco <mmngreco@gmail.com>
2023-11-02 19:33:56 +0100

    update

--- init.lua
+++ init.lua
@@ -545,11 +545,11 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<C-l>'] = cmp.mapping.complete(),
-    -- ['<C-l>'] = cmp.mapping.confirm {
-    --   behavior = cmp.ConfirmBehavior.Replace,
-    --   select = true,
-    -- },
+    -- ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-l>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
   },
   sources = {
     { name = 'nvim_lsp' },
