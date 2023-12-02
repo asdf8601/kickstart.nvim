ccae397b79ee96bf13abd8a50d029387eeacc59b
Maximiliano Greco <mmngreco@gmail.com>
2023-12-02 23:33:01 +0100

    Auto commit

--- init.lua
+++ init.lua
@@ -493,7 +493,7 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-n>'] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
     ['<C-p>'] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-    ['<tab>'] = cmp.mapping.complete(),
+    -- ['<tab>'] = cmp.mapping.complete(),
     ['<C-l>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
