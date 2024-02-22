b7f8e7de555f1392c33919806bbb62079cd6ae81
Maximiliano Greco <mmngreco@gmail.com>
2024-02-22 20:19:05 +0100

    Auto commit

--- init.lua
+++ init.lua
@@ -291,8 +291,7 @@ vim.keymap.set('n', '<leader>fk', ':Telescope keymaps<cr>', { noremap = true, de
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim',
-      'bash' },
+    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'http', 'json', 'yaml' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
