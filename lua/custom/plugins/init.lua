5bb45a09395e549b05f22461e1e625be752f3dec
Maximiliano Greco <mmngreco@gmail.com>
2024-02-22 20:21:25 +0100

    Auto commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -274,7 +274,7 @@ return {
             -- stay in current windows (.http file) or change to results window (default)
             stay_in_current_window_after_split = false,
             -- Skip SSL verification, useful for unknown certificates
-            skip_ssl_verification = false,
+            skip_ssl_verification = true,
             -- Encode URL before making request
             encode_url = true,
             -- Highlight request on run
