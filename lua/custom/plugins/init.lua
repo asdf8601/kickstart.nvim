32a29cf2ac8b29de17a5e68766e812c485e66cd0
Maximiliano Greco <mmngreco@gmail.com>
2024-04-07 14:16:52 +0200

    Auto commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,7 +14,9 @@ return {
     name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
-      require('render-markdown').setup({})
+      require('render-markdown').setup({
+        headings = { '#', '##', '###', '####', '#####', '######' },
+      })
     end,
   },
 
