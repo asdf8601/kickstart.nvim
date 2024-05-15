e2c3d774de1a7ba9f14c02051ff03b00881f8c66
Maximiliano Greco <mmngreco@gmail.com>
2024-05-15 18:57:37 +0200

    Auto commit

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -130,10 +130,66 @@ return {
   {
     "robitx/gp.nvim",
     config = function()
-      require("gp").setup()
-      local config = require("gp").config
-      config.agents[0].model = "gpt-4o"
-      vim.print(config)
+
+      require("gp").setup({
+        agents = {
+          {
+            name = "ChatGPT4",
+            chat = true,
+            command = false,
+            model = { model = "gpt-4o", temperature = 1.1, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a general AI assistant.\n\n"
+              .. "The user provided the additional info about how they would like you to respond:\n\n"
+              .. "- If you're unsure don't guess and say you don't know instead.\n"
+              .. "- Ask question if you need clarification to provide better answer.\n"
+              .. "- Think deeply and carefully from first principles step by step.\n"
+              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+              .. "- Use Socratic method to improve your thinking and coding skills.\n"
+              .. "- Don't elide any code from your output if the answer requires coding.\n"
+              .. "- Take a deep breath; You've got this!\n",
+          },
+          {
+            name = "ChatGPT3-5",
+            chat = true,
+            command = false,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo", temperature = 1.1, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are a general AI assistant.\n\n"
+              .. "The user provided the additional info about how they would like you to respond:\n\n"
+              .. "- If you're unsure don't guess and say you don't know instead.\n"
+              .. "- Ask question if you need clarification to provide better answer.\n"
+              .. "- Think deeply and carefully from first principles step by step.\n"
+              .. "- Zoom out first to see the big picture and then zoom in to details.\n"
+              .. "- Use Socratic method to improve your thinking and coding skills.\n"
+              .. "- Don't elide any code from your output if the answer requires coding.\n"
+              .. "- Take a deep breath; You've got this!\n",
+          },
+          {
+            name = "CodeGPT4",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-4o", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+          {
+            name = "CodeGPT3-5",
+            chat = false,
+            command = true,
+            -- string with model name or table with model name and parameters
+            model = { model = "gpt-3.5-turbo", temperature = 0.8, top_p = 1 },
+            -- system prompt (use this to specify the persona/role of the AI)
+            system_prompt = "You are an AI working as a code editor.\n\n"
+              .. "Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n"
+              .. "START AND END YOUR ANSWER WITH:\n\n```",
+          },
+        },
+      })
       -- https://github.com/Robitx/gp.nvim?tab=readme-ov-file#4-configuration
     end,
   },
