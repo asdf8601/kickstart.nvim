ee4a1af0565cd70536e660af6882eee44dcac79a
Maximiliano Greco <mmngreco@gmail.com>
2024-06-16 16:19:45 +0200

    Auto commit

--- init.lua
+++ init.lua
@@ -1213,4 +1213,17 @@ vim.cmd.colorscheme 'modus_vivendi'
 
 vim.opt.foldlevelstart = 99
 vim.opt.foldlevel = 99
+
+
+-- Run gofmt + goimports on save
+
+local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+   require('go.format').goimports()
+  end,
+  group = format_sync_grp,
+})
+
 -- vim: ts=2 sts=2 sw=2 et tw=0
