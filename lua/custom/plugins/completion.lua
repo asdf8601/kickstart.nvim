b266f6865e01428af87878e59d3bb2311e960dac
Max G <6231413+asdf8601@users.noreply.github.com>
2025-11-01 16:23:40 +0100

    update

--- lua/custom/plugins/completion.lua
+++ lua/custom/plugins/completion.lua
@@ -1,3 +1,18 @@
+local build_snippets = function()
+  local ls = require('luasnip')
+  local ps = ls.parser.parse_snippet
+  ls.filetype_extend("mdx",   { "md" })
+  ls.filetype_extend("astro", { "md" })
+
+  ls.add_snippets("md", { ps("mer", "<Mermaid>\n\t$1\n</Mermaid>") })
+  ls.add_snippets("md", { ps("dl", "<dl>\n\t<dt>$1</dt>\n\t<dd>$2</dd>\n</dl>") })
+  ls.add_snippets("md", { ps("dt", "<dt>$1</dt>\n<dd>$2</dd>") })
+  ls.add_snippets("md", { ps("gi", "<GridItem cols={1}>\n\t$1\n</GridItem>") })
+  ls.add_snippets("md", { ps("gr", "<Grid cols={2}>\n\t$1\n</Grid>") })
+  ls.add_snippets("md", { ps("card", "<Card>\n\t$1\n</Card>") })
+end
+
+
 return {
   { -- Autocompletion
     'saghen/blink.cmp',
@@ -5,6 +20,18 @@ return {
     version = '1.*',
     optional = true,
     dependencies = {
+      {
+        'L3MON4D3/LuaSnip',
+        version = 'v2.*',
+        opts = {
+            history = true,
+            delete_check_events = 'TextChanged',
+        },
+        config = function (_, opts)
+          require('luasnip').setup(opts)
+          build_snippets()
+        end,
+      },
       'fang2hou/blink-copilot',
       'Kaiser-Yang/blink-cmp-avante',
       'disrupted/blink-cmp-conventional-commits',
@@ -15,20 +42,38 @@ return {
     },
     opts = {
       appearance = { nerd_font_variant = 'mono' },
-      completion = { documentation = { auto_show = true, auto_show_delay_ms = 200 } },
+      completion = { documentation = { auto_show = true, auto_show_delay_ms = 100 } },
       keymap = {
         preset = 'default',
-        ['<C-k>'] = nil,
+        -- ['<C-k>'] = nil,
         -- ['<C-q>'] = { function(cmp) cmp.show { providers = { 'jira' } } end, },
         -- ['<C-g><C-g>'] = { function(cmp) cmp.show { providers = { 'copilot' } } end, },
       },
       sources = {
+        default = { 'snippets', 'lsp', 'path', 'buffer', 'copilot' },
         -- default = { 'lsp', 'path', 'buffer', 'avante', 'cvcm', 'env' },
-        default = { 'jira', 'copilot', 'lsp', 'path', 'buffer', 'avante', 'cvcm', 'git', 'env' },
+        -- default = { 'snippets', 'lsp', 'path', 'buffer', 'jira', 'copilot', 'avante', 'cvcm', 'git', 'env' },
+        -- default = { 'snippets', 'lsp', 'path', 'buffer', 'jira', 'copilot', 'avante', 'cvcm', 'git', 'env' },
         providers = {
+          buffer = {
+            -- make it cheap and faster
+            min_keyword_length = 3,
+            max_items = 50,
+            score_offset = -5,
+            async = true,
+            opts = {
+              -- solo el buffer actual (no todos los visibles)
+              get_bufnrs = function() return { vim.api.nvim_get_current_buf() } end,
+              -- corta tamaños grandes
+              max_total_buffer_size = 200000,  -- 200KB
+              max_async_buffer_size = 150000,  -- por buffer
+              use_cache = true,
+            },
+          },
           jira = {
             name = 'blink-cmp-jira',
             module = 'blink-cmp-jira',
+            async = true,
             opts = {
               trigger_character = 'GC-',
               jira_project = 'GC',
@@ -41,10 +86,12 @@ return {
             module = 'blink-cmp-git',
             name = 'Git',
             opts = {},
+            async = true,
           },
           env = {
             name = 'Env',
             module = 'blink-cmp-env',
+            async = true,
             opts = {
               -- item_kind = require('blink.cmp.types').CompletionItemKind.Variable,
               show_braces = false,
@@ -71,8 +118,12 @@ return {
           },
         },
       },
-      -- snippets = { preset = 'luasnip' },
-      fuzzy = { implementation = 'lua' },
+      snippets = { preset = 'luasnip' },
+      fuzzy = {
+        -- implementation = 'lua'
+        -- implementation = 'prefer_rust',
+        implementation = 'prefer_rust_with_warning',
+      },
       signature = { enabled = true },
     },
   },
--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -234,52 +234,6 @@ return {
   },
 
   { 'stevanmilic/nvim-lspimport' },
-  { -- Autoformat
-    'stevearc/conform.nvim',
-    event = { 'BufWritePre' },
-    cmd = { 'ConformInfo' },
-    keys = {
-      {
-        '<leader>f',
-        function()
-          require('conform').format { async = true, lsp_format = 'fallback' }
-        end,
-        mode = '',
-        desc = '[F]ormat buffer',
-      },
-    },
-    opts = {
-      notify_on_error = false,
-      formatters_by_ft = {
-        bash = { 'shfmt' },
-        css = { { 'prettierd', 'prettier' } },
-        docker = { 'dockerfmt' },
-        go = { 'gofumpt', 'goimports' },
-        html = { { 'prettierd', 'prettier' } },
-        javascript = { { 'prettierd', 'prettier' } },
-        json = { { 'prettierd', 'prettier' } },
-        lua = { 'stylua' },
-        python = { 'autoimport_fix', 'ruff_fix', 'ruff_format', 'docformatter' },
-        sh = { 'shfmt' },
-        sql = { 'sqlfmt' },
-        yaml = { 'prettier' },
-        -- markdown = { { 'prettierd', 'prettier' } },
-        -- ['*'] = { 'codespell' }, -- apply to all
-        -- ['_'] = { 'trim_whitespace' }, -- for the rest
-      },
-      formatters = {
-        autoimport_fix = {
-          command = 'autoimport',
-          args = { '$FILENAME' },
-          stdin = false,
-          condition = function(_, ctx)
-            return vim.fn.executable 'autoimport' == 1 and ctx.filename:match '%.py$'
-          end,
-          exit_codes = { 0 },
-        },
-      },
-    },
-  },
   {
     'ThePrimeagen/harpoon',
     init = function()
